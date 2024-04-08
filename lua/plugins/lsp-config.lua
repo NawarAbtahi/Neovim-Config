356e977d0a7c8c512cee585a68e56c044605f68f
BuildsPixld <125456278+BuildsPixld@users.noreply.github.com>
2024-04-08 16:49:45 +0600

    Add files via upload

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "clangd",
+          "html",
+          "pyright"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.pyright.setup ({
+        capabilities = capabilities
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    end
+  }
+}
